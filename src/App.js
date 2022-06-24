564c00379614209791c77de05d45f16123d03673
Elia El Atram <72523490+eliaatram@users.noreply.github.com>
2022-06-24 20:02:04 +0200

    setup routes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 
 import React from 'react'
+import { Route, Routes } from 'react-router-dom';
+import Login from './Pages/Login';
+import Signup from './Pages/Signup';
 
 const App = () => {
   return (
     <div className="App">
-      Hello World
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+      </Routes>
     </div>
   );
 }
