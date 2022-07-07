936d23067227938e6aa3429e975031274055e108
AnthonyAjami <anthony.ajami@epita.fr>
2022-07-07 02:10:17 +0200

    candidate card component

--- src/Pages/candidates/Candidates.jsx
+++ src/Pages/candidates/Candidates.jsx
@@ -1,7 +1,16 @@
 import './candidates.scss';
+import CandidateCard from '../../components/candidateComponent/candidateCard.jsx';
 
 const Candidates = () => {
-  return <div>Candidates</div>;
+  return (
+    <div>
+      <CandidateCard
+        imageUrl='https://i.kym-cdn.com/entries/icons/original/000/035/634/Emmanuel_Macron2.png'
+        viewOrVote='View'
+        name='emmanuel macron'
+        party='La république en Marche!' />
+    </div>
+  );
 };
 
 export default Candidates;
---
+++ src/components/candidateComponent/candidateCard.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import './candidateCard.scss';
+
+function CandidateCard(props) {
+    return (
+        <div className="candidate__card">
+            <div className="candidate__card__header">
+                <img className="candidate__image" src={props.imageUrl} alt="candidate-image" />
+                <button className='candidate__action__button'>{props.viewOrVote}</button>
+            </div>
+            <div className="candidate__card__body">
+                <div className="candidate__card__body__name">
+                    {props.name}
+                </div>
+                <div className="candidate__card__body__party">
+                    {props.party}
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default CandidateCard;
