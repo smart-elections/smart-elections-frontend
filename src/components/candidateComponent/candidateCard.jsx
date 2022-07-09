064ae95d8dd88abf0cd72c2d6d97c2d6d74aecf2
Elias Afara <afaraelias@gmail.com>
2022-07-09 16:13:20 +0200

    Modified Candidate Card

--- src/components/candidateComponent/candidateCard.jsx
+++ src/components/candidateComponent/candidateCard.jsx
@@ -1,23 +1,21 @@
 import React from 'react';
 import './candidateCard.scss';
 
-function CandidateCard(props) {
-    return (
-        <div className="candidate__card">
-            <div className="candidate__card__header">
-                <img className="candidate__image" src={props.imageUrl} alt="candidate-image" />
-                <button className='candidate__action__button' onClick={props.buttonAction}>View</button>
-            </div>
-            <div className="candidate__card__body">
-                <div className="candidate__card__body__name">
-                    {props.name}
-                </div>
-                <div className="candidate__card__body__party">
-                    {props.party}
-                </div>
-            </div>
-        </div>
-    );
+function CandidateCard({ imageUrl, buttonAction, name, party }) {
+  return (
+    <div className='candidate__card'>
+      <div className='candidate__card__header'>
+        <img className='candidate__image' src={imageUrl} alt='candidate' />
+        <button className='candidate__action__button' onClick={buttonAction}>
+          View
+        </button>
+      </div>
+      <div className='candidate__card__body'>
+        <div className='candidate__card__body__name'>{name}</div>
+        <div className='candidate__card__body__party'>{party}</div>
+      </div>
+    </div>
+  );
 }
 
 export default CandidateCard;
