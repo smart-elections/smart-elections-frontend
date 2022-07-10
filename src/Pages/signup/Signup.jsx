90c22bc073a592a6296a287ccfd410b1b4ec3feb
Elias Afara <afaraelias@gmail.com>
2022-07-11 01:24:35 +0200

    Added MetaMask Modal logic and refacoted code

--- src/Pages/signup/Signup.jsx
+++ src/Pages/signup/Signup.jsx
@@ -1,104 +1,83 @@
 import { useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import { toast } from 'react-toastify';
+import axios from 'axios';
+
+// Styles
 import './signup.scss';
+
+// Assets
 import Logo from '../../assets/images/Logo.png';
+
+// Data
+import { signUpInputs } from '../../data/formInputs';
+
+// Components
 import FormInput from '../../components/formInput/FormInput';
-import { Link } from 'react-router-dom';
+import MetaMaskWallet from '../../components/metaMaskWallet/MetaMaskWallet';
+
+// utils
+import { signUpFormValidation } from '../../utils/formValidations';
 
 const initialState = {
-  socialSecurityNumber: '',
+  ssn: '',
+  firstName: '',
+  lastName: '',
+  username: '',
   password: '',
-  confirmPassword: '',
-  metaMaskAddress: '',
 };
 
 const Signup = () => {
   const [formValues, setFormValues] = useState(initialState);
   const [formErrors, setFormErrors] = useState({});
+  const navigate = useNavigate();
 
-  const inputs = [
-    {
-      id: 1,
-      name: 'socialSecurityNumber',
-      type: 'text',
-      placeholder: 'Social Security Number',
-      required: true,
-    },
-    {
-      id: 2,
-      name: 'password',
-      type: 'password',
-      placeholder: 'Password',
-      required: true,
-    },
-    {
-      id: 3,
-      name: 'confirmPassword',
-      type: 'password',
-      placeholder: 'Confirm Password',
-      required: true,
-    },
-    {
-      id: 4,
-      name: 'metaMaskAddress',
-      type: 'text',
-      placeholder: 'MetaMask Wallet Address',
-      required: true,
-    },
-  ];
+  // metamask modal state and functions
+  const [open, setOpen] = useState(false);
+
+  const handleOpenMetaMaskFormModal = () => {
+    setOpen(true);
+  };
+  const handleClose = (event, reason) => {
+    // Do nothing if the user clicks outside the modal
+    if (reason && reason === 'backdropClick') return;
+    setOpen(false);
+    navigate('/elections');
+  };
 
   const handleSubmit = async (e) => {
     e.preventDefault();
 
     const noErrors = Object.values(formErrors).every((err) => err === '');
     if (noErrors) {
-      // TODO: call API to login
-      console.log(formErrors);
+      // TODO: test out the axios call
+      // calling the backend api 'signup' to sign up the user
+      await axios
+        .put('/accounts/signup', formValues)
+        .then((response) => {
+          console.log(response);
+          console.log(response.data);
+          toast.success('Account created successfully!');
+
+          // TODO: Add the user to local storage
+          // localStorage.setItem(
+          //   'SmartElectionsProfile',
+          //   JSON.stringify(response.data)
+          // );
+
+          // when registration is successful, open the metamask modal to add the metamask wallet then after that navigate to the elections page
+          handleOpenMetaMaskFormModal();
+        })
+        .catch((error) => {
+          console.error('Sign up form: There was an error!', error);
+        });
     }
   };
 
   const onInputChange = (e) => {
     let { name, value } = e.target;
     setFormValues({ ...formValues, [name]: value });
-    validateInput(name, value);
-  };
-
-  // ✅ Validate input
-  // TODO: Add more validation rules
-  const validateInput = (name, value) => {
-    setFormErrors((prev) => {
-      const stateObj = { ...prev, [name]: '' };
-
-      switch (name) {
-        case 'socialSecurityNumber':
-          if (!(value.length === 13)) {
-            stateObj[name] = 'Social Security Number must be of 13 characters';
-          }
-          break;
-
-        case 'password':
-          if (value.length < 12) {
-            stateObj[name] = 'Password must consist of at least 12 characters';
-          }
-          break;
-
-        case 'confirmPassword':
-          if (formValues.password && value !== formValues.password) {
-            stateObj[name] = 'Password and Confirm Password does not match';
-          }
-          break;
-
-        case 'metaMaskAddress':
-          if (!(value.length === 42)) {
-            stateObj[name] = 'Invalid MetaMask Wallet Address';
-          }
-          break;
-
-        default:
-          break;
-      }
-
-      return stateObj;
-    });
+    signUpFormValidation(setFormErrors, name, value);
   };
 
   return (
@@ -112,7 +91,7 @@ const Signup = () => {
         />
       </div>
       <form className='Auth-form' onSubmit={handleSubmit}>
-        {inputs.map((input) => (
+        {signUpInputs.map((input) => (
           <FormInput
             key={input.id}
             {...input}
@@ -133,6 +112,7 @@ const Signup = () => {
           </Link>
         </div>
       </form>
+      <MetaMaskWallet open={open} handleClose={handleClose} />
     </div>
   );
 };
