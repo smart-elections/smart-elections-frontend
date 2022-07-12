5c481ab44b1936159b4625ccfd52f2f9ad4f9863
Elias Afara <afaraelias@gmail.com>
2022-07-12 21:39:59 +0200

    Updated according to private and public routes

--- src/App.js
+++ src/App.js
@@ -9,6 +9,9 @@ import './Styles/colors.scss';
 import 'react-toastify/dist/ReactToastify.css';
 import { DarkModeContext } from './context/darkModeContext';
 
+// Routes
+import { PrivateRoute, PublicRoute } from './Routes/Routes';
+
 // Pages
 import Login from './Pages/login/Login';
 import Signup from './Pages/signup/Signup';
@@ -29,6 +32,30 @@ const App = () => {
   const { darkMode, dispatch } = useContext(DarkModeContext);
   const [componentMounted, setComponentMounted] = useState(false);
 
+  const [isAuthenticated, setIsAuthenticated] = useState(
+    JSON.parse(localStorage.getItem('SmartElectionsProfile'))
+      ?.isAuthenticated || false
+  );
+
+  useEffect(() => {
+    if (JSON.parse(localStorage.getItem('SmartElectionsProfile')) === null) {
+      window.localStorage.setItem(
+        'SmartElectionsProfile',
+        JSON.stringify({ isAuthenticated: false })
+      );
+    }
+
+    const userProfile = JSON.parse(
+      window.localStorage.getItem('SmartElectionsProfile')
+    );
+
+    if (userProfile !== null) {
+      if (userProfile.isAuthenticated) {
+        setIsAuthenticated(true);
+      }
+    }
+  }, [isAuthenticated]);
+
   useEffect(() => {
     const localTheme = window.localStorage.getItem('smartVotingTheme');
     if (localTheme) {
@@ -48,50 +75,75 @@ const App = () => {
     <BrowserRouter>
       <div className={darkMode ? 'app dark' : 'app'}>
         <ToastContainer />
-        <Sidebar />
+        {isAuthenticated && <Sidebar />}
         <div className='appContainer'>
-          <Navbar />
+          {isAuthenticated && <Navbar />}
           <div className='routesWrapper'>
             <Routes>
               <Route path='/'>
-                <Route index element={<Analysis />} />
-                <Route path='login' element={<Login />} />
+                <Route
+                  element={<PrivateRoute isAuthenticated={isAuthenticated} />}
+                >
+                  <Route index element={<Analysis />} />
+                </Route>
 
-                <Route path='register' element={<Signup />} />
+                <Route
+                  element={<PublicRoute isAuthenticated={isAuthenticated} />}
+                >
+                  <Route path='login' element={<Login />} />
+                </Route>
 
-                {/* 
-          OTP verification
-          <Route path='verify-otp' element={<VerifyOTP />} />
-
-          MetaMask wallet
-          <Route path='metamask-wallet' element={<Wallet />} />
-          
-          */}
+                <Route
+                  element={<PublicRoute isAuthenticated={isAuthenticated} />}
+                >
+                  <Route path='register' element={<Signup />} />
+                </Route>
 
                 <Route path='elections'>
-                  <Route index element={<Elections />} />
                   <Route
-                    path='voting/:year/:electionId'
-                    element={<ElectionVoting />}
-                  />
-                  <Route
-                    path='analytics/:year/:electionId'
-                    element={<ElectionAnalytics />}
-                  />
+                    element={<PrivateRoute isAuthenticated={isAuthenticated} />}
+                  >
+                    <Route index element={<Elections />} />
+                    <Route
+                      path='voting/:year/:electionId'
+                      element={<ElectionVoting />}
+                    />
+                    <Route
+                      path='analytics/:year/:electionId'
+                      element={<ElectionAnalytics />}
+                    />
+                  </Route>
                 </Route>
 
                 <Route path='candidates'>
-                  <Route index element={<Candidates />} />
-                  <Route path=':candidateId' element={<SingleCandidate />} />
+                  <Route
+                    element={<PrivateRoute isAuthenticated={isAuthenticated} />}
+                  >
+                    <Route index element={<Candidates />} />
+                    <Route path=':candidateId' element={<SingleCandidate />} />
+                  </Route>
                 </Route>
 
-                <Route path='profile' element={<UserProfile />} />
-                <Route path='news' element={<News />} />
+                <Route
+                  element={<PrivateRoute isAuthenticated={isAuthenticated} />}
+                >
+                  <Route path='profile' element={<UserProfile />} />
+                </Route>
 
                 <Route
-                  path='accounts/password/change'
-                  element={<ChangePassword />}
-                />
+                  element={<PrivateRoute isAuthenticated={isAuthenticated} />}
+                >
+                  <Route exact path='news' element={<News />} />
+                </Route>
+
+                <Route
+                  element={<PrivateRoute isAuthenticated={isAuthenticated} />}
+                >
+                  <Route
+                    path='accounts/password/change'
+                    element={<ChangePassword />}
+                  />
+                </Route>
               </Route>
 
               <Route path='*' element={<NotFound />} />
