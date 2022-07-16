6d8beb3accd323cce453260a78e9728944f2c9ec
AnthonyAjami <anthony.ajami@epita.fr>
2022-07-16 02:26:45 +0200

    switched news source to french news api

--- src/Pages/news/News.jsx
+++ src/Pages/news/News.jsx
@@ -10,7 +10,7 @@ const News = () => {
   useEffect(() => {
     axios
       .get(
-        `http://newsapi.org/v2/everything?q=rich&from=2022-07-09&sortBy=publishedAt&apiKey=${apiKey}`
+        `https://newsdata.io/api/1/news?apikey=pub_92485c26ad795af824315c649f12fc032dce&country=fr&language=fr&category=politics`
       )
       .then((response) => setData(response.data))
       .catch((error) => console.log(error));
@@ -21,9 +21,9 @@ const News = () => {
       <div className='news__container' id='style-scrollbar'>
         <div className='all__news'>
           {data
-            ? data.articles.map((news) => (
-                <NewsArticle data={news} key={news.url} />
-              ))
+            ? data.results.map((news) => (
+              <NewsArticle data={news} key={news.link} />
+            ))
             : 'Loading'}
         </div>
       </div>
@@ -37,22 +37,22 @@ const NewsArticle = ({ data }) => {
       <div className='horizontal-child-left'>
         <img
           className='news__image'
-          src={data.urlToImage}
-          alt={data.source.name}
+          src={data.image_url}
+          alt={data.title}
           height={160}
           width={250}
         />
       </div>
       <div className='horizontal-child-right'>
         <div className='news__text'>
-          <p className='news__category'>{data.source.name}</p>
+          <p className='news__source'>{data.source_id}</p>
           <p className='news__title'>
             {data.title.length > 140
               ? `${data.title.substring(0, 140)}...`
               : data.title}
           </p>
           <p className='news__published'>
-            Published {data.publishedAt.slice(0, 10)}
+            Published {data.pubDate.slice(0, 10)}
           </p>
         </div>
       </div>
