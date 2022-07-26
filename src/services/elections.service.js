6d01f01efbaf110891477de9305eb1e7dcf791f5
Elias Afara <afaraelias@gmail.com>
2022-07-26 19:11:24 +0200

    Implemented elections cadidate & voting services

---
+++ src/services/elections.service.js
@@ -0,0 +1,65 @@
+import axios from 'axios';
+import { toast } from 'react-toastify';
+
+export const getElectionCandidates = async (
+  electionYear,
+  electionType,
+  electionRound
+) => {
+  try {
+    const response = await axios.get(
+      `/elections/candidates?year=${electionYear}&type=${electionType}&round=${electionRound}`
+    );
+    toast.success('Election candidates fetched fetched successfully');
+
+    return response.data;
+  } catch (error) {
+    console.error('Error getting elections: ', error);
+  }
+};
+
+// Calls Metamask to connect wallet on clicking Connect Wallet button
+export const connectWallet = async (
+  setCorrectNetwork,
+  correctNetwork,
+  setCurrentAccount
+) => {
+  try {
+    const { ethereum } = window;
+
+    if (!ethereum) {
+      toast.error('Metamask not detected');
+      return;
+    }
+
+    checkCorrectNetwork(setCorrectNetwork);
+
+    if (correctNetwork) {
+      const accounts = await ethereum.request({
+        method: 'eth_requestAccounts',
+      });
+
+      setCurrentAccount(accounts[0]);
+    } else {
+      return;
+    }
+  } catch (error) {
+    console.log('Error connecting to metamask', error);
+  }
+};
+
+// Checks if wallet is connected to the goerli network
+export const checkCorrectNetwork = async (setCorrectNetwork) => {
+  const { ethereum } = window;
+  let chainId = await ethereum.request({ method: 'eth_chainId' });
+
+  const goerliChainId = '0x5';
+
+  if (chainId !== goerliChainId) {
+    toast.error('You are not connected to the Goerli Testnet!');
+    setCorrectNetwork(false);
+  } else {
+    toast.success('You are connected to the Goerli Testnet!');
+    setCorrectNetwork(true);
+  }
+};
