ab9fdaf40787a8d9eddcc2c81169fa7260d95011
Elias Afara <afaraelias@gmail.com>
2022-07-27 04:20:21 +0200

    Implemented election services

--- src/services/elections.service.js
+++ src/services/elections.service.js
@@ -1,18 +1,45 @@
 import axios from 'axios';
 import { toast } from 'react-toastify';
 
-export const getElections = async (dispatch) => {
+export const getElections = async () => {
   try {
-    const response = await axios.get('/elections/');
-    console.log(response);
-    toast.success('Elections fetched successfully');
-    // dispatch({
-    //   type: 'GetElections',
-    //   payload: response.data.data,
-    // });
+    const response = await axios.get('/elections');
 
     return response.data;
   } catch (error) {
     console.error('Error getting elections: ', error);
+  }
+};
+
+export const getElectionCandidates = async (
+  electionYear,
+  electionType,
+  electionRound
+) => {
+  try {
+    const response = await axios.get(
+      `/elections/candidates?year=${electionYear}&type=${electionType}&round=${electionRound}`
+    );
+    toast.success('Election candidates fetched fetched successfully');
+
+    return response.data;
+  } catch (error) {
+    console.error('Error getting elections: ', error);
+  }
+};
+
+export const getElectionWinner = async (
+  electionYear,
+  electionType,
+  electionRound
+) => {
+  try {
+    const response = await axios.get(
+      `analytics/election_winner?year=${electionYear}&type=${electionType}&round=${electionRound}`
+    );
+
+    return response;
+  } catch (error) {
+    console.error('Error getting election winner: ', error);
   }
 };
