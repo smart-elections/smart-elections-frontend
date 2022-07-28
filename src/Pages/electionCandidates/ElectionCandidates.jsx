3851d080f771d144db985440818fa1f3ba3f77e4
Elias Afara <afaraelias@gmail.com>
2022-07-28 21:16:35 +0200

    Added Breadcrumbs

--- src/Pages/electionCandidates/ElectionCandidates.jsx
+++ src/Pages/electionCandidates/ElectionCandidates.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { useParams } from 'react-router-dom';
+import { useParams, useNavigate } from 'react-router-dom';
 import './electionCandidates.scss';
 
 // components
@@ -9,11 +9,16 @@ import CandidateProfileModal from '../../components/candidateProfileModal/Candid
 // services
 import { getElectionCandidates } from '../../services/candidates.services';
 
+import Typography from '@mui/material/Typography';
+import Breadcrumbs from '@mui/material/Breadcrumbs';
+
 const ElectionCandidates = () => {
   const [candidates, setCandidates] = useState([]);
   const [selectedCandidate, setSelectedCandidate] = useState({});
-  const { electionYear, electionRound, electionType } = useParams();
   const [loadingCandidates, setLoadingCandidates] = useState(true);
+
+  const { electionYear, electionRound, electionType } = useParams();
+  const navigate = useNavigate();
 
   const [open, setOpen] = useState(false);
 
@@ -49,6 +54,26 @@ const ElectionCandidates = () => {
           handleClose={handleClose}
         />
       )}
+      <div role='presentation' style={{ marginLeft: '10px' }}>
+        <Breadcrumbs aria-label='breadcrumb'>
+          <Typography
+            className='breadCrumb-typography'
+            onClick={() => navigate(-1)}
+          >
+            Elections
+          </Typography>
+          <Typography color='text.primary'>
+            {electionRound === 1 ? '1st' : '2nd'}{' '}
+            {electionType === 1
+              ? 'Presidential'
+              : electionType === 2
+              ? 'Legislative'
+              : 'Local'}
+            {' election - '}
+            {electionYear}
+          </Typography>
+        </Breadcrumbs>
+      </div>
       <div className='candidatesContainer'>
         {!loadingCandidates ? (
           candidates?.map((candidate) => (
