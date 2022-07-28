d92a9401f57c4a92d9551f79ebfb32fc36dfb303
Elias Afara <afaraelias@gmail.com>
2022-07-28 20:03:28 +0200

    Changed election select code and styling

--- src/Pages/analysis/Analysis.jsx
+++ src/Pages/analysis/Analysis.jsx
@@ -5,6 +5,14 @@ import axios from 'axios';
 import AnalyticsCard from '../../components/analyticsComponent/AnalyticsCard.jsx';
 import './analysis.scss';
 
+import Divider from '@mui/material/Divider';
+
+import Box from '@mui/material/Box';
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+import FormControl from '@mui/material/FormControl';
+import Select from '@mui/material/Select';
+
 import BarChart from '../../components/analyticsComponent/BarChart.js';
 
 const baseUrl = 'http://ec2-44-202-30-87.compute-1.amazonaws.com:8000';
@@ -16,124 +24,185 @@ var yyyy = today.getFullYear();
 today = yyyy + '-' + mm + '-' + dd;
 
 const Analysis = () => {
-
   const [latestElections, setLatestElections] = useState([]);
 
   const [selectedElection, setSelectedElection] = useState('');
 
-  const [selectedElectionRegisteredVoters, setSelectedElectionRegisteredVoters] = useState(0);
-  const [selectedElectionRegisteredVotersPercentage, setSelectedElectionRegisteredVotersPercentage] = useState(0);
+  const [
+    selectedElectionRegisteredVoters,
+    setSelectedElectionRegisteredVoters,
+  ] = useState(0);
+  const [
+    selectedElectionRegisteredVotersPercentage,
+    setSelectedElectionRegisteredVotersPercentage,
+  ] = useState(0);
 
   const [selectedElectionVoters, setSelectedElectionVoters] = useState(0);
-  const [selectedElectionVotersPercentage, setSelectedElectionVotersPercentage] = useState(0);
+  const [
+    selectedElectionVotersPercentage,
+    setSelectedElectionVotersPercentage,
+  ] = useState(0);
 
   const [selectedElectionWinner, setSelectedElectionWinner] = useState('');
-  const [selectedElectionWinnerVotes, setSelectedElectionWinnerVotes] = useState(0);
-  const [selectedElectionWinnerPercentage, setSelectedElectionWinnerPercentage] = useState(0);
-  const [selectedElectionWinnerParty, setSelectedElectionWinnerParty] = useState('');
-  const [selectedElectionWinnerImage, setSelectedElectionWinnerImage] = useState('');
+  const [selectedElectionWinnerVotes, setSelectedElectionWinnerVotes] =
+    useState(0);
+  const [
+    selectedElectionWinnerPercentage,
+    setSelectedElectionWinnerPercentage,
+  ] = useState(0);
+  const [selectedElectionWinnerParty, setSelectedElectionWinnerParty] =
+    useState('');
+  const [selectedElectionWinnerImage, setSelectedElectionWinnerImage] =
+    useState('');
 
   const [chartData, setChartData] = useState([]);
 
   useEffect(() => {
     const fetchElectionsData = async () => {
-      const { data: { data } } = await axios(`${baseUrl}/elections?end='${today}'`);
+      const {
+        data: { data },
+      } = await axios(`${baseUrl}/elections?end='${today}'`);
       setLatestElections(data);
-      setSelectedElection(`year=${data[0].election_year}&round=${data[0].election_round}&type=${data[0].election_type}`);
+      setSelectedElection(
+        `year=${data[0].election_year}&round=${data[0].election_round}&type=${data[0].election_type}`
+      );
 
-      console.log('1st', `year=${data[0].election_year}&round=${data[0].election_round}&type=${data[0].election_type}`)
+      console.log(
+        '1st',
+        `year=${data[0].election_year}&round=${data[0].election_round}&type=${data[0].election_type}`
+      );
 
       const fetchRegisteredData = async () => {
-        const response1 = await axios(`${baseUrl}/analytics/registered_voters?year=${data[0].election_year}&round=${data[0].election_round}&type=${data[0].election_type}`);
+        const response1 = await axios(
+          `${baseUrl}/analytics/registered_voters?year=${data[0].election_year}&round=${data[0].election_round}&type=${data[0].election_type}`
+        );
         setSelectedElectionRegisteredVoters(response1.data.registered_voters);
-        setSelectedElectionRegisteredVotersPercentage(response1.data.lastElectionDifference);
+        setSelectedElectionRegisteredVotersPercentage(
+          response1.data.lastElectionDifference
+        );
 
         console.log(response1.data);
-      }
+      };
       fetchRegisteredData();
 
       const fetchVotesData = async () => {
-        const response2 = await axios(`${baseUrl}/analytics/voters?year=${data[0].election_year}&round=${data[0].election_round}&type=${data[0].election_type}`);
+        const response2 = await axios(
+          `${baseUrl}/analytics/voters?year=${data[0].election_year}&round=${data[0].election_round}&type=${data[0].election_type}`
+        );
         setSelectedElectionVoters(response2.data.voters);
-        setSelectedElectionVotersPercentage(response2.data.lastElectionDifference);
+        setSelectedElectionVotersPercentage(
+          response2.data.lastElectionDifference
+        );
         console.log(response2.data);
-      }
+      };
       fetchVotesData();
 
       const fetchWinnerData = async () => {
-        const response3 = await axios(`${baseUrl}/analytics/election_winner?year=${data[0].election_year}&round=${data[0].election_round}&type=${data[0].election_type}`);
-        setSelectedElectionWinner(response3.data.winner.citizen_firstname + ' ' + response3.data.winner.citizen_lastname);
+        const response3 = await axios(
+          `${baseUrl}/analytics/election_winner?year=${data[0].election_year}&round=${data[0].election_round}&type=${data[0].election_type}`
+        );
+        setSelectedElectionWinner(
+          response3.data.winner.citizen_firstname +
+            ' ' +
+            response3.data.winner.citizen_lastname
+        );
         setSelectedElectionWinnerVotes(response3.data.winner.votes);
         setSelectedElectionWinnerPercentage(response3.data.percentage);
         setSelectedElectionWinnerParty(response3.data.winner.candidate_party);
         setSelectedElectionWinnerImage(response3.data.winner.candidate_image);
-      }
+      };
       fetchWinnerData();
 
       const fetchChartData = async () => {
-        const response4 = await axios(`${baseUrl}/analytics/election_winner?year=${data[0].election_year}&round=${data[0].election_round}&type=${data[0].election_type}`);
+        const response4 = await axios(
+          `${baseUrl}/analytics/election_winner?year=${data[0].election_year}&round=${data[0].election_round}&type=${data[0].election_type}`
+        );
         setChartData(response4.data.results);
-      }
+      };
       fetchChartData();
-
-    }
+    };
     fetchElectionsData();
   }, []);
 
   const onElectionChange = async (e) => {
     console.log(e.target.value);
 
-    let response = await axios(`${baseUrl}/analytics/registered_voters?${e.target.value}`);
+    setSelectedElection(e.target.value);
+
+    let response = await axios(
+      `${baseUrl}/analytics/registered_voters?${e.target.value}`
+    );
 
     console.log('on election change', response.data);
 
     setSelectedElectionRegisteredVoters(response.data.registered_voters);
-    setSelectedElectionRegisteredVotersPercentage(response.data.lastElectionDifference);
+    setSelectedElectionRegisteredVotersPercentage(
+      response.data.lastElectionDifference
+    );
 
-    let response2 = await axios(`${baseUrl}/analytics/voters?${e.target.value}`);
+    let response2 = await axios(
+      `${baseUrl}/analytics/voters?${e.target.value}`
+    );
 
     setSelectedElectionVoters(response2.data.voters);
     setSelectedElectionVotersPercentage(response2.data.lastElectionDifference);
 
-    let response3 = await axios(`${baseUrl}/analytics/election_winner?${e.target.value}`);
+    let response3 = await axios(
+      `${baseUrl}/analytics/election_winner?${e.target.value}`
+    );
 
-    setSelectedElectionWinner(response3.data.winner.citizen_firstname + ' ' + response3.data.winner.citizen_lastname);
+    setSelectedElectionWinner(
+      response3.data.winner.citizen_firstname +
+        ' ' +
+        response3.data.winner.citizen_lastname
+    );
     setSelectedElectionWinnerVotes(response3.data.winner.votes);
     setSelectedElectionWinnerPercentage(response3.data.percentage);
     setSelectedElectionWinnerParty(response3.data.winner.candidate_party);
     setSelectedElectionWinnerImage(response3.data.winner.candidate_image);
 
-    let response4 = await axios(`${baseUrl}/analytics/election_winner?${e.target.value}`);
+    let response4 = await axios(
+      `${baseUrl}/analytics/election_winner?${e.target.value}`
+    );
 
     setChartData(response4.data.results);
-
-  }
+  };
 
   return (
     <div className='analysis__container'>
-      <div className='selectInput'>
-        <div className='selectBox'>
-          <FilterListIcon className='icon' />
-          <select
-            className='select'
-            aria-label='Election select'
-            id='selectedElection'
-            name='selected_election'
-            onChange={onElectionChange}
-            required
-          >
-            {latestElections.map((election) => {
-              return (
-                <option key={election.election_id}
-                  value={`year=${election.election_year}&type=${election.election_type}&round=${election.election_round}`}>
-                  {election.election_type === 1 ? 'Presidential' : election.election_type === 2 ? 'Legislative' : 'Local'} {election.election_year} {election.election_round === 1 ? '1st' : '2nd'}
-                </option>
-              )
-            }
-            )}
-          </select>
-        </div>
+      <div className='electionSelectContainer'>
+        <Box sx={{ minWidth: 150 }}>
+          <FormControl fullWidth>
+            <InputLabel id='selectedElection-label'>Election</InputLabel>
+            <Select
+              labelId='selectedElection-label'
+              id='selectedElection'
+              label='Election'
+              value={selectedElection}
+              onChange={onElectionChange}
+            >
+              {latestElections.map((election) => {
+                return (
+                  <MenuItem
+                    key={election.election_id}
+                    value={`year=${election.election_year}&round=${election.election_round}&type=${election.election_type}`}
+                  >
+                    {election.election_type === 1
+                      ? 'Presidential'
+                      : election.election_type === 2
+                      ? 'Legislative'
+                      : 'Local'}{' '}
+                    {election.election_year}{' '}
+                    {election.election_round === 1 ? '1st' : '2nd'}
+                  </MenuItem>
+                );
+              })}
+            </Select>
+          </FormControl>
+        </Box>
       </div>
+
+      <Divider />
       <div className='analysis__component'>
         <div className='analysis__component__left'>
           <div className='analysis__component__left__upper'>
@@ -156,8 +225,7 @@ const Analysis = () => {
           </div>
           <div className='analysis__component__left__lower'>
             <div className='analysis__component__left__lower--graph'>
-              <BarChart
-                data={chartData} />
+              <BarChart data={chartData} />
             </div>
           </div>
         </div>
@@ -165,9 +233,7 @@ const Analysis = () => {
           <div className='analysis__component__right--winner'>
             <div className='winner__card'>
               <div className='winner__card__text--box'>
-                <div className='winner__card__title'>
-                  Winner
-                </div>
+                <div className='winner__card__title'>Winner</div>
                 <div className='winner__card__name'>
                   {selectedElectionWinner}
                 </div>
@@ -176,7 +242,11 @@ const Analysis = () => {
                 </div>
               </div>
               <div className='winner__card__image--box'>
-                <img className='winner__card__image' src={selectedElectionWinnerImage} alt='winner' />
+                <img
+                  className='winner__card__image'
+                  src={selectedElectionWinnerImage}
+                  alt='winner'
+                />
               </div>
             </div>
           </div>
@@ -191,7 +261,7 @@ const Analysis = () => {
         </div>
       </div>
     </div>
-  )
+  );
 };
 
 export default Analysis;
