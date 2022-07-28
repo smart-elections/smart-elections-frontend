8aecf5c66042a2e4fb59caad1c2e99e002c67d68
Elias Afara <afaraelias@gmail.com>
2022-07-28 18:07:05 +0200

    clean up

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -2,8 +2,6 @@ import React from 'react';
 import { Navigate, Outlet } from 'react-router-dom';
 import useAppStateContext from '../hooks/useAppStateContext';
 
-// TODO: CHECK WHETHER THE USER IS AUTHENTICATED OR NOT
-
 export const PrivateRoute = () => {
   const { appState } = useAppStateContext();
 
