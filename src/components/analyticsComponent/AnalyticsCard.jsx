35976f8c4d3bd06747788aab641bc375dcaa805f
AnthonyAjami <anthony.ajami@epita.fr>
2022-07-28 19:46:13 +0200

    Fixed warnings

--- src/components/analyticsComponent/AnalyticsCard.jsx
+++ src/components/analyticsComponent/AnalyticsCard.jsx
@@ -7,7 +7,7 @@ function AnalyticsCard({ title, number, percentage, color }) {
             <div className={color === 'white' ? 'analytics__card__upper' : 'analytics__card__green__upper'}>
                 <div className={color === 'white' ? 'analytics__card__upper__title' : 'analytics__card__green__upper__title'}>{title}</div>
                 <div className={color === 'white' ? percentage > 0 ? 'analytics__card__upper__comparison--increase' : 'analytics__card__upper__comparison--decrease' : 'analytics__card__green__upper__comparison'}>
-                    {percentage === 'No previous elections' ? '' : percentage > 0 ? '+' + `${percentage} %` : '' + `${percentage} %`}
+                    {percentage === 'No previous elections' ? '' : percentage > 0 ? `+${percentage} %` : `${percentage} %`}
                 </div>
             </div>
             <div className={color === 'white' ? 'analytics__card__lower' : 'analytics__card__green__lower'}>
